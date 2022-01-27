04dcf157a5a593350d9632eec213dfc357f534e4
jadanis <joshallendanis@gmail.com>
2022-01-27 11:16:27 -0500

    Create Main.hs

---
+++ src/Main.hs
@@ -0,0 +1,14 @@
+import UserInteract (getBool, getInt)
+import FactorInt (showFactor)
+
+main :: IO ()
+main = factorInt >> continue
+
+continue :: IO ()
+continue = (putStrLn "Factor another number?") >> getBool >>= (\b -> if b then main else end)
+
+end :: IO ()
+end = return ()
+
+factorInt :: IO ()
+factorInt = getInt >>= (putStrnLn . showFactor)
